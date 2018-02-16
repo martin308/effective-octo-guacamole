187f9065dfc1e0c6da9cb35f1d2b1577a8f7e662
Martin Holman <martin308@gmail.com>
2018-02-16 13:11:41 -0800

    Sinatra add Bugsnag

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
+require 'bugsnag'
 
 set :bind, '0.0.0.0'
 
+Bugsnag.configure do |config|
+  config.api_key = "APIKEY"
+end
+
+use Bugsnag::Rack
+
 get '/' do
-  'Hello world!'
+  raise "nope"
 end
